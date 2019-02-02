7945c4b8607d5bfb1e0d994fd5a20a7dd868c475
Nato Boram <nato.boram@gmail.com>
2019-02-02 18:15:08 -0500

    white icon blue background

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
       home: Scaffold(
         appBar: AppBar(
           title: Text(_platform + " Device Info"),
