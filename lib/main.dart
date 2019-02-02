c921c77369a85dc02d9d4441959769875122f51f
Nato Boram <nato.boram@gmail.com>
2019-02-02 14:30:07 -0500

    created home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,124 @@
-import 'package:flutter/material.dart';
+import "dart:async";
+import "dart:io";
+import "package:device_info/device_info.dart";
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
-void main() => runApp(MyApp());
+void main() {
+  runZoned(() {
+    runApp(MyApp());
+  }, onError: (dynamic error, dynamic stack) {
+    print(error);
+    print(stack);
+  });
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  Future<void> initPlatformState() async {
+    Map<String, dynamic> deviceData;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    try {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        "Error :": "Failed to get platform version."
+      };
+    }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    if (!mounted) return;
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _deviceData = deviceData;
     });
   }
 
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      "version.securityPatch": build.version.securityPatch,
+      "version.sdkInt": build.version.sdkInt,
+      "version.release": build.version.release,
+      "version.previewSdkInt": build.version.previewSdkInt,
+      "version.incremental": build.version.incremental,
+      "version.codename": build.version.codename,
+      "version.baseOS": build.version.baseOS,
+      "board": build.board,
+      "bootloader": build.bootloader,
+      "brand": build.brand,
+      "device": build.device,
+      "display": build.display,
+      "fingerprint": build.fingerprint,
+      "hardware": build.hardware,
+      "host": build.host,
+      "id": build.id,
+      "manufacturer": build.manufacturer,
+      "model": build.model,
+      "product": build.product,
+      "supported32BitAbis": build.supported32BitAbis,
+      "supported64BitAbis": build.supported64BitAbis,
+      "supportedAbis": build.supportedAbis,
+      "tags": build.tags,
+      "type": build.type,
+      "isPhysicalDevice": build.isPhysicalDevice,
+      "androidId": build.androidId,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      "name": data.name,
+      "systemName": data.systemName,
+      "systemVersion": data.systemVersion,
+      "model": data.model,
+      "localizedModel": data.localizedModel,
+      "identifierForVendor": data.identifierForVendor,
+      "isPhysicalDevice": data.isPhysicalDevice,
+      "utsname.sysname:": data.utsname.sysname,
+      "utsname.nodename:": data.utsname.nodename,
+      "utsname.release:": data.utsname.release,
+      "utsname.version:": data.utsname.version,
+      "utsname.machine:": data.utsname.machine,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              Platform.isAndroid ? "Android Device Info" : "iOS Device Info"),
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          children: _deviceData.keys.map((String property) {
+            return ListTile(
+              title: Text(property),
+              subtitle: Text(
+                "${_deviceData[property]}",
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          }).toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
